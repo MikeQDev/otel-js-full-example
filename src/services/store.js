a3df4e9675713a35495a6b18ee338b3f7cf94e3d
mike <m@home>
2022-11-06 17:13:04 -0500

    add cache and histogram to store

--- src/services/store.js
+++ src/services/store.js
@@ -1,9 +1,21 @@
+const { trace } = require('@opentelemetry/api');
+const { metrics } = require('@opentelemetry/api-metrics');
 const axios = require('axios');
 const express = require('express');
 const app = express();
 const PORT = 21469;
 
 const inventoryHost = 'http://localhost:21470';
+const useCache = true; // example to reduce latency by serving /getItem/:id from cache
+
+const cache = {};
+
+const requestLatencyHistogram = metrics
+  .getMeter('default')
+  .createHistogram('getItemRequestLatency', {
+    description: 'time taken to serve request',
+    unit: 'milliseconds',
+  });
 
 // NOTE: receiving a 404 response sets the GET request span status from axios to error
 // Solutions:
@@ -34,22 +46,39 @@ app.get('/allItems', async function (req, res) {
 });
 
 app.get('/getItem/:id', async function (req, res) {
+  const startTime = new Date();
   const productId = req.params.id;
-  await axios
-    .get(`${inventoryHost}/products/${productId}`)
-    .then((resp) => {
-      const product = resp.data;
-      res.send({ name: product.name, price: product.price, inStock: product.quantity > 0 });
-    })
-    .catch((e) => {
-      // no need to record exception:
-      // HTTP auto instrumentations for axios already record exceptions in span
-      if (e.response?.status === 404) {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(500);
-      }
-    });
+  const currentSpan = trace.getActiveSpan();
+  if (useCache && productId in cache && new Date() - cache[productId].cacheTime < 10000) {
+    currentSpan.setAttribute('cacheHit', true);
+    currentSpan.addEvent('Cache hit - returning from cache instead of calling inventory service');
+    res.send(cache[productId]); // ideally, you wouldn't want to include cache timestamp in response
+    requestLatencyHistogram.record(new Date() - startTime, { usedCache: true });
+  } else {
+    await axios
+      .get(`${inventoryHost}/products/${productId}`)
+      .then((resp) => {
+        const product = resp.data;
+        const transformedResponse = {
+          name: product.name,
+          price: product.price,
+          inStock: product.quantity > 0,
+        };
+        currentSpan.setAttribute('cacheHit', false); // span operations must occur before sending response
+        res.send(transformedResponse);
+        cache[productId] = { ...transformedResponse, cacheTime: new Date().getTime() };
+        requestLatencyHistogram.record(new Date() - startTime, { usedCache: false });
+      })
+      .catch((e) => {
+        // no need to record exception:
+        // HTTP auto instrumentations for axios already record exceptions in span
+        if (e.response?.status === 404) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(500);
+        }
+      });
+  }
 });
 
 app.listen(PORT, () => {
